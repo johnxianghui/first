b3ffeeb25a2527fb7e20d21f12799adcdc7035d5
向辉 <15994217357@163.com>
2017-08-13 13:36:50 +0800

    新建文件夹

---
+++ changeCollectionView/changeCollectionCell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  changeCollectionCell
+//
+//  Created by 向辉 on 2017/8/13.
+//  Copyright © 2017年 hui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ changeCollectionView/changeCollectionCellTests/changeCollectionCellTests.m
@@ -0,0 +1,39 @@
+//
+//  changeCollectionCellTests.m
+//  changeCollectionCellTests
+//
+//  Created by 向辉 on 2017/8/13.
+//  Copyright © 2017年 hui. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface changeCollectionCellTests : XCTestCase
+
+@end
+
+@implementation changeCollectionCellTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
